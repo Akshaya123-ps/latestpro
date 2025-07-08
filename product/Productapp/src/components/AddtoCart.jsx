ca80fbeb4d0ff164a5bbba9a42b70cc62076148f
Akshaya123-ps <144515328+Akshaya123-ps@users.noreply.github.com>
2025-07-08 21:35:37 +0530

    Update AddtoCart.jsx

--- product/Productapp/src/components/AddtoCart.jsx
+++ product/Productapp/src/components/AddtoCart.jsx
@@ -1,8 +1,10 @@
-import { Card, CardContent, CardMedia, Typography } from '@mui/material'
-import React, { useState } from 'react'
+import { Button, Card, CardContent, CardMedia, Grid, Typography } from '@mui/material'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 
 const AddtoCart = () => {
+  const user= JSON.parse(localStorage.getItem('user'));
   var [cartItems,setCartitems]=useState([])
     console.log("loc",location.state)
     useEffect(()=>{
@@ -12,6 +14,19 @@ const AddtoCart = () => {
   })
   .catch((err)=> console.log(err));
       },[])
+       const handleRemove = (cartItemId) => {
+        axios.delete(`http://localhost:3002/delcart/${cartItemId}`)
+      .then((res) => {
+        console.log(res.data.message)
+       alert(res.data.message)
+       
+        setCartitems(prevItems => prevItems.filter(item => item._id !== cartItemId));
+      })
+      .catch((err) => {
+        console.log(err);
+        // You might want to add user feedback here
+      });
+  };
   return (
     <div>
       <Grid container spacing ={2}>
@@ -32,8 +47,9 @@ const AddtoCart = () => {
         <Typography gutterBottom variant="h5" component="div">{item.productId.Pname} </Typography>
          <Typography  variant="h5" sx={{ color: 'text.secondary' }}>₹{item.productId.price} </Typography>
         <Typography variant="body2" sx={{ color: 'text.secondary' }}> {item.productId.Disc} </Typography>
+       
       </CardContent>
-      
+       <Button variant='contained' color="error"onClick={() => handleRemove(item._id)} sx={{ margin: 2 }}>Remove</Button>
     </Card>
     </Grid>
      )
